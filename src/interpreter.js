f9dd5c885e7065f32a833717a4f01a339676c7f9
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2025-02-04 17:49:29 -0800

    fixing bugs

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,28 +1,32 @@
+// snake interpreter
+
 export default function interpret(match) {
   const grammar = match.matcher.grammar;
 
-  const memory = {};
+  const memory = new Map();
 
-  const interpreter = grammar.createSemantics().addOperation("interpret", {
+  const interpreter = grammar.createSemantics().addOperation("eval", {
     Program(statements) {
       for (const statement of statements.children) {
         statement.eval();
       }
     },
-    Statement_assign(id, _eq, exp, _semicolin) {
+    Statement_assign(id, _eq, exp, _semi) {
       memory.set(id.sourceString, exp.eval());
     },
-    Statement_print(_print, exp, _semicolin) {
+    Statement_print(_print, exp, _semi) {
       console.log(exp.eval());
     },
-    // number(digits) {
-    //   return Number(digits.sourceString);
-    // },
-    id(first, rest) {
-      const name = first.sourceString + rest.sourceString;
+    number(_digits, _period, _decimals, _e, _unary, _exponent) {
+      return Number(this.sourceString);
+    },
+    id(_first, _rest) {
+      const name = this.sourceString;
+      if (!memory.has(name)) {
+        throw new Error(`Undefined variable: ${name}`);
+      }
       return memory.get(name);
     },
-    //...
   });
 
   throw interpreter(match).eval();
--- src/parser.js
+++ src/parser.js
@@ -1,16 +1,16 @@
-import * as fs from 'fs';
-import * as ohm from 'ohm-js';
+import * as fs from "fs";
+import * as ohm from "ohm-js";
 
-const grammar = ohm.grammar(fs.readFileSync('src/snake.ohm'));
+const grammar = ohm.grammar(fs.readFileSync("src/snake.ohm", "utf8"));
 
 export default function parse(source) {
-    const sourceCode = fs.readFileSync(source, 'utf8');
-    
-    const match = grammar.match(sourceCode);
-    
-    if (match.succeeded()) {
-        return match;
-    } else {
-        throw new Error(match.message);
-    }
+  const sourceCode = fs.readFileSync(source, "utf8");
+
+  const match = grammar.match(sourceCode);
+
+  if (match.succeeded()) {
+    return match;
+  } else {
+    throw new Error(match.message);
+  }
 }
