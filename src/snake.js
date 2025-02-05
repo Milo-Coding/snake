2f0ecf9f2b9f4557928f274ac7fc767dee701813
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2025-02-04 17:49:11 -0800

    cleaning up + commenting

--- src/snake.js
+++ src/snake.js
@@ -1,10 +1,6 @@
-import * as fs from "fs";
-import * as ohm from "ohm-js";
+// snake interpreter
 import parse from "./parser.js";
 import interpret from "./interpreter.js";
-
-// read the contents of the file snake.ohm into a string
-const grammar = ohm.grammar(fs.readFileSync("src/snake.ohm", "utf8"));
 
 if (process.argv.length !== 3) {
   console.error("Usage: node src/snake.js <source>");
@@ -12,9 +8,11 @@ if (process.argv.length !== 3) {
 }
 
 try {
+  // Syntax
   const match = parse(process.argv[2]);
+  // Semantics
   interpret(match);
 } catch (error) {
-  console.error(error.message);
+  console.error(error);
   process.exit(1);
 }
