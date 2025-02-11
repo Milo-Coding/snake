f1890ea0cd71a5997f6fdf9d64f3fc8b5334f6d7
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2025-02-11 14:49:58 -0800

    commenting out cassowary-style translator

--- src/translator.js
+++ src/translator.js
@@ -22,74 +22,65 @@ export default function translate(match) {
         statement.translate();
       }
     },
-    Statement_while(_while, _open, condition, _close, statement) {
-      emit("while (", condition.translate(), ") {");
-      statement.translate();
-      emit("}");
-    },
-    // BStatment_break(_break, _semi) {
-    //   emit("break;");
+    // Statement_while(_while, _open, condition, _close, statement) {
+    //   emit("while (", condition.translate(), ") {");
+    //   statement.translate();
+    //   emit("}");
     // },
-    // VarDecl(_type, id, _eq, exp, _semi) {
-    //   check(!locals.has(id.sourceString), `Variable ${id.sourceString} already declared`, id);
+    // Statement_assign(id, _eq, exp, _semi) {
     //   const initializer = exp.translate();
-    //   locals.set(id.sourceString, "number");
-    //   emit(`let ${id.sourceString} = ${initializer};`);
+    //   emit(id.sourceString, "=", initializer);
     // },
-    Statement_assign(id, _eq, exp, _semi) {
-      const initializer = exp.translate();
-      emit(id.sourceString, "=", initializer);
-    },
-    Statement_print(_print, exp, _semi) {
-      emit(`console.log(${exp.translate()});`);
-    },
-    number(_digits, _period, _decimals, _e, _unary, _exponent) {
-      return Number(this.sourceString);
-    },
-    id(_first, _rest) {
-      const name = this.sourceString;
-      check(locals.has(name), `Undefined variable ${name}`, this);
-      return name;
-    },
-    Exp_binary(left, op, right) {
-      const targetOp =
-        {
-          "==": "===",
-          "!=": "!==",
-          "<": "<",
-          "<=": "<=",
-          ">": ">",
-          ">=": ">=",
-        }?.[op.sourceString] || op.sourceString;
-      return `${left.translate()} ${targetOp} ${right.translate()}`;
-    },
-    Condition_binary(left, op, right) {
-      switch (op.sourceString) {
-        case "+":
-          return `${left.translate()} + ${right.translate()}`;
-        case "-":
-          return `${left.translate()} - ${right.translate()}`;
-      }
-    },
-    Term_binary(left, op, right) {
-      switch (op.sourceString) {
-        case "*":
-          return `${left.translate()} * ${right.translate()}`;
-        case "/":
-          return `${left.translate()} / ${right.translate()}`;
-        case "%":
-          return `${left.translate()} % ${right.translate()}`;
-      }
-    },
-    Factor_negation(_op, operand) {
-      return `-${operand.translate()}`;
-    },
-    Factor_binary(left, op, right) {
-      return `${left.translate()} ** ${right.translate()}`;
-    },
-    Primary_parens(_open, exp, _close) {
-      return `(${exp.translate()})`;
-    },
+    // Statement_print(_print, exp, _semi) {
+    //   emit(`console.log(${exp.translate()});`);
+    // },
+    // number(_digits, _period, _decimals, _e, _unary, _exponent) {
+    //   return Number(this.sourceString);
+    // },
+    // id(_first, _rest) {
+    //   const name = this.sourceString;
+    //   check(locals.has(name), `Undefined variable ${name}`, this);
+    //   return name;
+    // },
+    // Exp_binary(left, op, right) {
+    //   const targetOp =
+    //     {
+    //       "==": "===",
+    //       "!=": "!==",
+    //       "<": "<",
+    //       "<=": "<=",
+    //       ">": ">",
+    //       ">=": ">=",
+    //     }?.[op.sourceString] || op.sourceString;
+    //   return `${left.translate()} ${targetOp} ${right.translate()}`;
+    // },
+    // Condition_binary(left, op, right) {
+    //   switch (op.sourceString) {
+    //     case "+":
+    //       return `${left.translate()} + ${right.translate()}`;
+    //     case "-":
+    //       return `${left.translate()} - ${right.translate()}`;
+    //   }
+    // },
+    // Term_binary(left, op, right) {
+    //   switch (op.sourceString) {
+    //     case "*":
+    //       return `${left.translate()} * ${right.translate()}`;
+    //     case "/":
+    //       return `${left.translate()} / ${right.translate()}`;
+    //     case "%":
+    //       return `${left.translate()} % ${right.translate()}`;
+    //   }
+    // },
+    // Factor_negation(_op, operand) {
+    //   return `-${operand.translate()}`;
+    // },
+    // Factor_binary(left, op, right) {
+    //   return `${left.translate()} ** ${right.translate()}`;
+    // },
+    // Primary_parens(_open, exp, _close) {
+    //   return `(${exp.translate()})`;
+    // },
   });
 
   translator(match).translate();
