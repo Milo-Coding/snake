f8c8c12aee202c9106fd44ab9af1e7f5df0a92a2
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2025-02-12 13:20:34 -0800

    lots of progress

--- src/translator.js
+++ src/translator.js
@@ -24,20 +24,35 @@ export default function translate(match) {
         statement.translate();
       }
     },
-    Stmt_print(_print, args, _newline) {
-      emit(`console.log(${args.translate()});`);
+    Stmt_declaration(dec, _newline) {
+      dec.translate();
     },
-    Stmt_declaration(_dec, _newline) {},
     VarDec(type, id, _eq, exp) {
       const initializer = exp?.translate();
       locals.set(id.sourceString, type.sourceString);
       emit(`let ${id.sourceString} = ${initializer};`);
     },
-    Args(args) {
-      return args
-        .asIteration()
-        .children.map((arg) => arg.translate())
-        .join(", ");
+    Stmt_assignment(Assignment, _newline) {
+      emit(`${Assignment.translate()};`);
+    },
+    Stmt_print(_print, args, _newline) {
+      emit(`console.log(${args.translate()});`);
+    },
+    Stmt_while(_while, exp, block) {
+      emit(`while (${exp.translate()}) {`);
+      block.translate();
+      emit(`}`);
+    },
+    Assignment(id, _eq, exp) {
+      check(
+        locals.has(id.sourceString),
+        `Undefined variable ${id.sourceString}`,
+        id
+      );
+      return `${id.sourceString} = ${exp.translate()}`;
+    },
+    Block(_open, statements, _close) {
+      statements.translate();
     },
     Exp(args) {
       return args
@@ -57,7 +72,7 @@ export default function translate(match) {
         .children.map((arg) => arg.translate())
         .join(" ** ");
     },
-    Exp3(left, op, right) {
+    Exp3_binary(left, op, right) {
       const targetOp =
         {
           "=?": "===",
@@ -66,8 +81,8 @@ export default function translate(match) {
           "<=?": "<=",
           ">?": ">",
           ">=?": ">=",
-        }?.[op.sourceString] || op.sourceString;
-      return `${left.translate()}${targetOp}${right.translate()}`;
+        }[op.sourceString] || op.sourceString;
+      return `${left.translate()} ${targetOp} ${right.translate()}`;
     },
     Exp4(args) {
       return args
@@ -87,15 +102,32 @@ export default function translate(match) {
     Exp6(args) {
       return args.translate();
     },
+    Args(args) {
+      return args
+        .asIteration()
+        .children.map((arg) => arg.translate())
+        .join(", ");
+    },
     numberlit(_digits, _period, _decimals, _e, _unary, _exponent) {
       return Number(this.sourceString);
     },
     stringlit(_letter, _chars) {
       return `${this.sourceString}`;
     },
+    newline(_) {
+      return "";
+    },
     _iter(...children) {
       return children.map((child) => child.translate());
     },
+    // id(_first, _rest) {
+    //   const name = this.sourceString;
+    //   check(locals.has(name), `Undefined variable ${name}`, this);
+    //   return name;
+    // },
+    // Factor_negation(_op, operand) {
+    //   return `-${operand.translate()}`;
+    // },
   });
 
   translator(match).translate();
