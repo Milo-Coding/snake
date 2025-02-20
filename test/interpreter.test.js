522109d8b324d7aea4f19eee65d0100c06d34051
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2025-02-20 13:46:08 -0800

    swapping grammar in interpreter test to match snake

--- test/interpreter.test.js
+++ test/interpreter.test.js
@@ -5,11 +5,11 @@ import parse from "../src/parser.js";
 describe("Interpreter", () => {
   it("it parses", () => {
     ok(parse("print 1\n").succeeded());
-    ok(parse("print -(dog ** 2) == -2500\n").succeeded());
+    ok(parse("print -(dog ^ 2) =? -2500\n").succeeded());
   });
   it("throws on syntax erorrs", () => {
     throws(() => parse("1d;1fsgref\n"), /Expected/);
-    throws(() => parse("print -(dog ** 2) == \n"), /Expected/);
+    throws(() => parse("print -(dog ^ 2) =? \n"), /Expected/);
     throws(() => parse(""), /Expected/);
   });
 });
