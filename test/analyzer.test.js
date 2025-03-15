a19ab8369985be7d968ed4e0d815ea041843cf3e
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2025-03-15 10:31:58 -0700

    adding analyzer test file

---
+++ test/analyzer.test.js
@@ -0,0 +1,8 @@
+import {describe, it} from "node:test"
+import { deepEqual } from "node:assert/strict"
+
+describe('Analyzer', () => {
+    it('it runs', () => {
+        deepEqual(1, 1);
+    });
+});
