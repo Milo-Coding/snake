de8df961dfde3ed02052ecb3cc9132482f7ba2ef
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2025-03-24 12:35:12 -0700

    renaming parser test file

--- test/parser.test.js
+++ test/parser.test.js
@@ -2,7 +2,7 @@ import { describe, it } from "node:test";
 import { ok, throws } from "node:assert/strict";
 import parse from "../src/parser.js";
 
-describe("Interpreter", () => {
+describe("Parser", () => {
   it("it parses", () => {
     ok(parse("print 1\n").succeeded());
     ok(parse("print -(dog ^ 2) =? -2500\n").succeeded());
