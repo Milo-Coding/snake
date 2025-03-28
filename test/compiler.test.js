e2a40e4d262cb6a458c1d8d8fdde646f887f3d38
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2025-03-27 17:37:59 -0700

    adding compiler tests

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,8 +1,15 @@
-import {describe, it} from "node:test"
-import { deepEqual } from "node:assert/strict"
+import { describe, it } from "node:test";
+import { deepEqual } from "node:assert/strict";
+// import compile from "../src/compiler.js";
 
-describe('Compiler', () => {
-    it('it runs', () => {
-        deepEqual(1, 1);
-    });
+describe("Compiler", () => {
+  it("it runs", () => {
+    deepEqual(1, 1);
+  });
+
+  // it compiles but is commented out because coverage is not yet implemented
+  //   it('compiles valid code to "parsed" output', () => {
+  //     const result = compile("print 1\n", "parsed");
+  //     deepEqual(result, "Syntax is ok");
+  //   });
 });
